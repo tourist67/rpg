51a316c5c3d7a38fe1f71a74247bbed24ad6fb7e
tourist67 <lin6.jordan@gmail.com>
2026-01-02 22:28:20 -0500

    added KeyListener

--- src/main/Gamepanel.java
+++ src/main/Gamepanel.java
@@ -1,7 +1,10 @@
 
 package main;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 public class Gamepanel extends JPanel implements Runnable {
@@ -16,7 +19,21 @@ public class Gamepanel extends JPanel implements Runnable {
 	final int screenWidth = tileSize * maxScreenCol;
 	final int screenHeight = tileSize * maxScreenRow; 
 
+	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
+
+	// PLAYER SETTINGS
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
+
+	public Gamepanel() {
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
+	}
 
 	public void startGameThread() {
 		gameThread = new Thread(this);
@@ -32,12 +49,29 @@ public class Gamepanel extends JPanel implements Runnable {
 	}
 
 	public void update() {
-		
+		if (keyH.upPressed) {
+			playerY -= playerSpeed;
+		}
+		if (keyH.downPressed) {
+			playerY += playerSpeed;
+		}
+		if (keyH.leftPressed) {
+			playerX -= playerSpeed;
+		}
+		if (keyH.rightPressed) {
+			playerX += playerSpeed;
+		}
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
+		Graphics2D g2 = (Graphics2D) g;
+ 
+		g2.setColor(Color.white);
+		g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+		g2.dispose();
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 		JFrame window = new JFrame("2D RPG Game");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setSize(800, 600);
+		window.setResizable(false);
+
+		Gamepanel gamePanel = new Gamepanel();
+		window.add(gamePanel);
+		window.pack();
+
 		window.setLocationRelativeTo(null); 
 		window.setVisible(true);
+
+		gamePanel.startGameThread();
 	}
 }
